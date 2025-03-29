6248d08fa7af77db592615e1793f990c6d62d828
rezanicgil <rezanicgil@gmail.com>
2025-03-29 16:08:44 +0300

    feat: add login form component and integrate Tailwind CSS for styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import LoginForm from './components/login/LoginForm';
 function App() {
   return (
-    <div className="App">
-      <p>Authentication app</p>
+    <div className="App font-mono">
+      <main>
+        <LoginForm />
+      </main>
     </div>
   );
 }
---
+++ frontend/src/components/login/LoginForm.tsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+
+const LoginForm: React.FC = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    setEmail("");
+    setPassword("");
+  };
+
+  return (
+    <div className="login-form w-full max-w-sm mx-auto mt-10 p-6 bg-white rounded shadow-md p-4 text-sm">
+      <h2 className="text-2xl text-center p-4">Welcome</h2>
+    <form onSubmit={handleSubmit}>
+      <div className="form-group font-bold p-2">
+        <label htmlFor="email">Email: </label>
+        <input 
+        className="border border-gray-300 focus:border-sky-500"
+        type="email"
+        id="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        required
+        />
+      </div>
+      <div className="form-group font-bold p-2">
+        <label htmlFor="password">Password: </label>
+        <input
+        className="border border-gray-300 focus:border-sky-500"
+        type="password"
+        id="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        required
+        />
+      </div>
+
+      <div className="form-group font-bold p-2 text-center">
+        <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow text-xs">
+            Login
+        </button>
+      </div>
+    </form>
+    </div>
+  );
+};
+
+export default LoginForm;
